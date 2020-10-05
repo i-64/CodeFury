72d0a1dade05b9fd046355c2ffa4316dbb3c82f8
Mrunal Ahire <mrunal.ahire@gmail.com>
2020-10-05 15:08:05 +0530

    organizeMeeting code restored till Oct4

--- meetingRooms/src/main/java/com/meetingRooms/dao/OrganizeMeetingDao.java
+++ meetingRooms/src/main/java/com/meetingRooms/dao/OrganizeMeetingDao.java
@@ -3,6 +3,13 @@
  */
 package com.meetingRooms.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.meetingRooms.entity.Meeting;
+import com.meetingRooms.entity.MeetingRoom;
 import com.meetingRooms.utility.ConnectionManager;
 import com.meetingRooms.utility.OrganizeMeetingDaoFactory;
 
@@ -13,5 +20,33 @@ import com.meetingRooms.utility.OrganizeMeetingDaoFactory;
  *
  */
 public class OrganizeMeetingDao implements OrganizeMeetingDaoInterface {
+
+	/**
+	 * filter meeting rooms according to the meeting organised by manager
+	 * 
+	 * @param meeting as asked by the manager
+	 * @return meetingRoomsList the list of available and suitable meeting rooms
+	 */
+	@Override
+	public ArrayList<MeetingRoom> filterMeetingRoomsDao(Meeting meeting) {
+		
+		ArrayList<MeetingRoom> meetingRoomsList = new ArrayList<>();
+		Connection con = null;
+		try {
+			con = ConnectionManager.getConnection();
+
+			PreparedStatement ps = con.prepareStatement("select * from MEETING_ROOMS where start_time<? and end_time<?");
+			
+			// TODO query
+			// TODO resultset -> arraylist
+			
+		}
+		catch (SQLException | ClassNotFoundException e) {
+			// TODO log exception to error.log
+			e.printStackTrace();
+		}
+		
+		return meetingRoomsList;
+	}
 	
 }
---
+++ meetingRooms/src/main/java/com/meetingRooms/utility/SortRoomsByAverageRating.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.meetingRooms.utility;
+
+import java.util.Comparator;
+
+import com.meetingRooms.entity.MeetingRoom;
+
+/**
+ * Comparator for sorting meeting in decreaing order
+ * of their average rating
+ * 
+ * @author Mrunal Ahire
+ *
+ */
+public class SortRoomsByAverageRating implements Comparator<MeetingRoom> {
+
+	@Override
+	public int compare (MeetingRoom room1, MeetingRoom room2) {
+		
+		return Double.compare(room1.getAverageRating(), room2.getAverageRating());
+	}
+
+}
