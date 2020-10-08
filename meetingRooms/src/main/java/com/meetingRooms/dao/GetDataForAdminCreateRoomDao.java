d2f3daad6b550ca2d4c3d01398c1529765820849
Ashutosh Danwe <ashutosh.danwe91297@gmail.com>
2020-10-08 23:57:53 +0530

    admin create room

--- meetingRooms/src/main/java/com/meetingRooms/dao/GetDataForAdminCreateRoomDao.java
+++ meetingRooms/src/main/java/com/meetingRooms/dao/GetDataForAdminCreateRoomDao.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.meetingRooms.entity.AmenitiesEntity;
+import com.meetingRooms.entity.MeetingRoomEntity;
 import com.meetingRooms.entity.MeetingTypes;
 
 /**
@@ -32,6 +33,107 @@ public class GetDataForAdminCreateRoomDao implements GetDataForAdminCreateRoomDa
 		 
 	} // end of constructor
 	
+	
+	@Override
+	public int createRoom ( MeetingRoomEntity entity ) {
+		
+		PreparedStatement ps;
+		
+		try ( Connection con = DriverManager.getConnection ( "jdbc:derby:c:/database/meetingRoomsDB", "admin", "admin" ) ) { // get connection to database
+			
+			int per_hour_cost = 0; // to calculate per hour cost			
+			
+				// prepare query to enter data into meeting_room database
+			
+			ps = con.prepareStatement ( "insert into MEETING_ROOM (unique_name, seating_capacity, per_hour_cost, total_meetings_conducted, created_by) values (?,?,?,?,?)" );
+			
+			ps.setString ( 1, entity.getUniqueName () );
+			ps.setInt ( 2, entity.getSeatingCapacity() );
+			
+			ps.setInt ( 3, per_hour_cost );
+			ps.setInt ( 4, 0 );
+			
+			ps.setString ( 5, entity.getCreated_by () );			
+			
+			if ( !( ps.executeUpdate () > 0) ) {
+			
+				return 0; // successful insertion				
+			}
+			
+				// insert into room_amenities all amenities
+			
+			for ( String temp : entity.getAmenityList() ) {
+				
+					// prepare query to enter data into ROOM AMENITIES database
+				
+				ps = con.prepareStatement ( "insert into ROOM_AMENITIES values (?, ?)" );
+				
+				ps.setString (1, entity.getUniqueName ());
+				ps.setString (2, temp);	
+				
+				if ( !( ps.executeUpdate () > 0 ) ) {
+					
+					return 0; // return 0 if unsuccessful insertion
+				}
+			}
+			
+				// get cost from table
+			
+			int size = entity.getAmenityList().size ();
+			size--;
+			
+			String query = "select SUM(credit) from amenities where ";
+			
+			for ( String temp : entity.getAmenityList() ) {
+				
+				query = query + "id = " + temp;
+				
+				if ( size != 0 ) {
+				
+					query = query + " or ";
+					size--;
+				}
+			}
+			
+			ps = con.prepareStatement ( query );
+			
+			ResultSet rs = ps.executeQuery ();	// get cost
+			
+			if ( rs.next () ) {
+				
+				per_hour_cost = rs.getInt ( 1 );
+				
+			} else {
+				
+				return 0;
+			}
+			
+			if ( entity.getSeatingCapacity () > 5 && entity.getSeatingCapacity () <= 10 ) {
+				
+				per_hour_cost += 10;
+			
+			} else if ( entity.getSeatingCapacity () > 10 ) {
+				
+				per_hour_cost += 20;
+			}
+			
+				// 	prepare query to enter data into ROOM AMENITIES database
+			
+			ps = con.prepareStatement ( "update MEETING_ROOM set per_hour_cost = " + per_hour_cost + "  where unique_name = '" + entity.getUniqueName () + "'" );
+		
+			if ( ps.executeUpdate () > 0 ) {
+				
+				return 1;
+			}
+			
+			
+		} catch (SQLException e ) {
+			
+			e.printStackTrace ();
+		}
+		
+		return 0;
+	}
 	
 	@Override
 	public List<MeetingTypes> getMeetingTypes () {
